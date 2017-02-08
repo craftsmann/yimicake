0a3243af391b4a8f666bcaaacd5f7dcefcd7a88e
craftsmann <1791502202@qq.com>
2017-02-09 00:07:06 +0800

    购物车cookie部分

--- frontend/controllers/GoodsController.php
+++ frontend/controllers/GoodsController.php
@@ -4,18 +4,58 @@
 namespace frontend\controllers;
 use common\models\Goods;
 use Yii;
+use yii\web\Cookie;
 
 class GoodsController extends BaseController
 {
+    //商品详情
     public function actionView(){
         $id = Yii::$app->request->get('id');
         $result  = Goods::find()->joinWith('cate')->where([Goods::tableName().'.id'=>$id])->asArray()->all();
         return $this->render('view',['res'=>$result]);
     }
+
+    //添加购物车
     public function actionAddcar(){
 
+        $num = Yii::$app->request->get('num');
+        $id  = Yii::$app->request->get('id');
+        $tmpArr = [];
+        $success = ['status'=>'success','message'=>'已经添加至购物车'];
+        $error   = ['status'=>'success','message'=>'添加购物车失败,请重试'];
         if(Yii::$app->user->isGuest){
-            echo '错误';
+            //不如果存在cookie
+            if(!Yii::$app->request->cookies->has('YIMICAKE')){
+                $tmpArr[] = ['num'=>$num,'id'=>$id];
+                $cookieName = 'YIMICAKE';
+                $cookieVal  = base64_encode(serialize($tmpArr));
+                Yii::$app->response->cookies->add(new Cookie([
+                    'name'  =>$cookieName,
+                    'value' =>$cookieVal
+                ]));
+                return json_encode($success);
+            }else{
+            //如果存在cookie
+               $data =  unserialize(base64_decode(Yii::$app->request->cookies->getValue('YIMICAKE')));
+               $tmpID = '';
+                foreach ($data as &$v){
+                   if($v['id'] == $id){
+                       $v['num']=$num;
+                   }else{$tmpID = $id;}
+               }
+                if($tmpID!==''){
+                    $data[] = ['num'=>$num,'id'=>$tmpID];
+                }
+                $cookieVal  = base64_encode(serialize($data));
+                Yii::$app->response->cookies->add(new Cookie([
+                    'name'  =>'YIMICAKE',
+                    'value' =>$cookieVal,
+                ]));
+                return json_encode($success);
+            }
+        }else{
+
+
         }
     }
 
--- frontend/web/static/js/obj_goods.js
+++ frontend/web/static/js/obj_goods.js
@@ -7,35 +7,45 @@
         this.url    = $(this).attr('href');
         this.obj_num = $('#s-num').val();
         this.obj_gid = $('#s-num').attr('index');
+        this.obj_add = $('.s-add');
+        this.obj_reduce = $('.s-reduce');
+        this.default_num = 1;
+
+
+        var reduceNum = function () {
+            --_this.default_num;
+            $('#s-num').val(_this.default_num);
+        };
+
+        var addNum   = function () {
+            ++_this.default_num;
+            $('#s-num').val(_this.default_num);
+        };
+
         return this.each(function(){
+
+            _this.obj_reduce.bind('click',function () {
+                _this.default_num < 2 ? alert('商品数量不能小于1'):reduceNum();
+            });
+
+            _this.obj_add.bind('click',function () {
+                _this.default_num > 14 ? alert('商品数量不能多于15'):addNum();
+            });
+
+            //ajax请求
             $(this).click(function (e) {
                 e.preventDefault();
                 $.ajax({
                     url:_this.url,
-                    data:{num:_this.obj_num,id:_this.obj_gid},
+                    data:{num:_this.default_num,id:_this.obj_gid},
                     success:function (data) {
                         window.alert(data);
                     },
                     error:function () {
-                        window.alert(请求错误);
+                        window.alert('请求错误,请重新尝试!');
                     }
                 });
             })
         });
     }
 })(jQuery);
-
-/**
- * 购物车加减
- */
-$(function () {
-    var btn_add    = $('.s-add');
-    var btn_reduce = $('.s-reduce');
-    var btn_gval   = parseInt($('#s-num').val());
-    btn_reduce.bind('click',function () {
-       btn_gval < 2 ? alert('商品数量不能小于1'): $('#s-num').val(--btn_gval);
-    });
-    btn_add.bind('click',function () {
-        btn_gval > 14 ? alert('商品数量不能多于15'): $('#s-num').val(++btn_gval);
-    });
-});
